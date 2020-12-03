5a398aaed1926f698aea905fbae4915566ddf88d
Mottelz <shachna@gmail.com>
2020-12-02 19:59:12 -0500

    Completed day 2

--- day-1/solution1.py
+++ day-1/solution1.py
@@ -1,18 +1,22 @@
+from typing import List
+YEAR = 2020
+
+
 # Part 1 solution
-def part1(numbers):
+def part1(numbers: List[int]):
     for n1 in range(0, len(numbers)):
         for n2 in range(n1+1, len(numbers)):
-            if numbers[n1] + numbers[n2] == 2020:
+            if numbers[n1] + numbers[n2] == YEAR:
                 print(f"n1: {numbers[n1]}, n2: {numbers[n2]}, sum: {numbers[n1]+numbers[n2]}, product: {numbers[n1]*numbers[n2]}")
                 break
 
 
 # Part 2 Solution
-def part2(numbers):
+def part2(numbers: List[int]):
     for n1 in range(0, len(numbers)):
         for n2 in range(n1+1, len(numbers)):
             for n3 in range(n2+1, len(numbers)):
-                if numbers[n1] + numbers[n2] + numbers[n3] == 2020:
+                if numbers[n1] + numbers[n2] + numbers[n3] == YEAR:
                     print(f"n1: {numbers[n1]}, n2: {numbers[n2]}, n3: {numbers[n3]} sum: {numbers[n1]+numbers[n2]+numbers[n3]}, product: {numbers[n1]*numbers[n2]*numbers[n3]}")
                     break
 
@@ -23,8 +27,8 @@ def main():
     with open('input.txt', 'r') as file:
         data = file.read().split()
 
-    # Convert to numbers and remove bigger than 2020
-    numbers = [eval(num) for num in data if eval(num) < 2020]
+    # Convert to numbers and remove bigger than year
+    numbers = [eval(num) for num in data if eval(num) < YEAR]
     part1(numbers)
     part2(numbers)
 
---
+++ day-2/solution1.py
@@ -0,0 +1,49 @@
+from typing import List
+
+
+# Read in file
+def read_in_files(filename: str) -> List[str]:
+    with open(filename) as file:
+        data = file.read().splitlines()
+    return data
+
+
+# Parse each line
+def parse_line(line: str) -> (int, int, str, str):
+    line = line.replace(':', '')
+    line = line.replace('-', ' ')
+    split_line = line.split()
+    return eval(split_line[0]), eval(split_line[1]), split_line[2], split_line[3]
+
+
+# Validate password for part I
+def validate_password_for_sleds(min_count: int, max_count: int, letter: str, password: str):
+    count = len([a for a in password if a == letter])
+    return min_count <= count <= max_count
+
+
+# Validate password for part II
+def validate_password_for_toboggan(pos1, pos2, letter, password):
+    return (password[pos1-1] == letter) ^ (password[pos2-1] == letter)
+
+
+# Solution to part 1
+def part1():
+    data = read_in_files("input.txt")
+    count = 0
+    for line in data:
+        min_val, max_val, letter, pword = parse_line(line)
+        if validate_password_for_sleds(min_val, max_val, letter, pword):
+            count += 1
+    print(count)
+
+
+# Solution to part 2
+def part2():
+    data = read_in_files("input.txt")
+    count = 0
+    for line in data:
+        pos1, pos2, letter, pword = parse_line(line)
+        if validate_password_for_toboggan(pos1, pos2, letter, pword):
+            count += 1
+    print(count)
