2251996891a35ee18de8e8128b7be15d7a3a035b
Mottelz <shachna@gmail.com>
2020-12-13 17:07:05 -0500

    Fixed loop logic error.

--- day-12/waypoint.py
+++ day-12/waypoint.py
@@ -1,3 +1,6 @@
+import math
+
+
 class ShipWithWaypoint:
     def __init__(self, boat_north=0, boat_east=0, waypoint_north=1, waypoint_east=10):
         self.__boat_north = boat_north
@@ -6,12 +9,16 @@ class ShipWithWaypoint:
         self.__waypoint_east = waypoint_east
 
     def turn_right(self, dgr: int):
-        for i in range((dgr % 90) + 1):
-            self.__waypoint_east, self.__waypoint_north = self.__waypoint_north, self.__waypoint_east * -1
+        for i in range(dgr//90):
+            x, y = self.__waypoint_east, self.__waypoint_north
+            x, y = y, x * -1
+            self.__waypoint_east, self.__waypoint_north = x, y
 
     def turn_left(self, dgr: int):
-        for i in range((dgr % 90) + 1):
-            self.__waypoint_east, self.__waypoint_north = self.__waypoint_north * -1, self.__waypoint_east
+        for i in range(dgr//90):
+            x, y = self.__waypoint_east, self.__waypoint_north
+            x, y = y * -1, x
+            self.__waypoint_east, self.__waypoint_north = x, y
 
     def move_north(self, units: int):
         self.__waypoint_north += units
